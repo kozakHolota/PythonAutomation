57f10e9820c3648d45b36ce5e4bedc98af9d792f
Павло Мриглоцький <kozak.holota@gmail.com>
2016-01-22 23:42:30 +0100

    Add template test class for keyword driven testing

---
+++ engine/TestTemplate.py
@@ -0,0 +1,26 @@
+'''
+Created on 22 січ. 2016
+
+@author: chmel
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testTemplate(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testTemplate']
+    unittest.main()
